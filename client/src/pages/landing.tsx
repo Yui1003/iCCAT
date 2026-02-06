a7cfdef71324f08e5ba2a2c417547bc09db20d37
Yui <j.jomarie1435@gmail.com>
2026-02-06 17:14:47 +0800

    V2.3.4

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -170,7 +170,7 @@ export default function Landing() {
               </Link>
             </div>
             <div className="text-xs text-muted-foreground" data-testid="text-version">
-              version:2.3.2
+              version:2.3.4
             </div>
           </div>
         </div>
