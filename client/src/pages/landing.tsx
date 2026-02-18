feff051b4c3a214800731313e3329787ff81ac3a
Yui <j.jomarie1435@gmail.com>
2026-02-18 11:49:55 +0800

    V2.8.8

--- client/src/pages/landing.tsx
+++ client/src/pages/landing.tsx
@@ -280,7 +280,7 @@ export default function Landing() {
               </Link>
             </div>
             <div className={`text-xs ${isDaytime ? 'text-black/70' : 'text-white/70'}`} data-testid="text-version">
-              version:2.8.7
+              version:2.8.8
             </div>
           </div>
         </div>
